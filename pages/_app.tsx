25b807e222e51b99a50e73c0d1eaed08222c10f5
choidy180 <choidy180@naver.com>
2022-10-06 12:42:04 +0900

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import GlobalStyle from '@src/styles/globalStyle';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const queryClient = new QueryClient();
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <RecoilRoot>
+                    <GlobalStyle/>
+                    <Component {...pageProps} />
+                </RecoilRoot>
+            </QueryClientProvider>
+        </>
+    )
 }
 
 export default MyApp
