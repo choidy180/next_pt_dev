f2a983cce8dfd335b7fed61802a023d6bb76d1c9
choidy180 <choidy180@naver.com>
2022-10-07 15:56:04 +0900

    commit

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,20 +2,29 @@ import * as React from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { isThemeAtom } from 'recoil/theme';
 import styled from 'styled-components';
+import { BsPlusLg } from "react-icons/bs";
 
 export const Navigation = () => {
     const isTheme = useRecoilValue(isThemeAtom);
-    const setDarkAtom = useSetRecoilState(isThemeAtom);
+    const [mbNavView, setMbNavView] = React.useState(true);
     return(
         <>
-            <h1 onClick={()=> setDarkAtom((e)=>!e)}>{isTheme ? "TRUE" : "FALSE"}</h1>
-            <Container>
-                <span onClick={()=> console.log(isTheme)}>공지사항</span>
+            <Container color={isTheme ? 'rgb(116, 185, 255)' : '#FFE616'}>
+                <span>공지사항</span>
                 <span>가이드</span>
                 <span>커뮤니티</span>
                 <span>미디어</span>
                 <span>고객지원</span>
             </Container>
+            <MbContainer style={mbNavView ? {marginLeft: '100vw'} : {}} color={isTheme ? 'rgb(116, 185, 255)' : '#FFE616'}>
+                <BsPlusLg style={mbNavView ? {display: 'none'} : { display: 'block'}} onClick={()=>setMbNavView(true)}/>
+                <div style={mbNavView ? { display: 'block'} : {display: 'none'}} onClick={()=>setMbNavView(false)}/>
+                <span>공지사항</span>
+                <span>가이드</span>
+                <span>커뮤니티</span>
+                <span>미디어</span>
+                <span>고객지원</span>
+            </MbContainer>
         </>
     )
 }
@@ -37,11 +46,11 @@ const Container = styled.div`
             position: absolute;
             content: '';
             left: 0;
-            bottom: 2px;
+            bottom: -3px;
             opacity: 0.7;
             width: 0%;
             height: 4px;
-            transition: all .15s ease-in-out;
+            transition: all .25s ease-in-out;
             background-color: ${props =>props.color};
         }
         &:hover{
@@ -52,5 +61,87 @@ const Container = styled.div`
     }
     @media (max-width: 1024px) {
         display: none;
+    }
+`
+
+const MbContainer = styled.div`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${props => props.color};
+    z-index: 99999999;
+    transition: all .4s ease-in-out;
+    background-color: #FFFFFF;
+    span{
+        font-size: 24px;
+        cursor: pointer;
+        &::before{
+            content: '';
+            position: absolute;
+            bottom: -4px;
+            width: 0;
+            height: 4px;
+            font-weight: 500;
+            background-color: ${props=>props.color};
+            transition: all ease-in-out .2s;
+        }
+        &:hover{
+            &::before{
+                width: 100%;
+            }
+        }
+    }
+    svg{
+        position: fixed;
+        top: 30px;
+        right: 24px;
+        font-size: 28px;
+        transform: rotate(45deg);
+        cursor: pointer;
+    }
+    div{
+        position: fixed;
+        top: 40px;
+        right: 24px;
+        width: 28px;
+        height: 3px;
+        border-radius: 4px;
+        background-color: #000000;
+        cursor: pointer;
+        &::before{
+            content: '';
+            position: absolute;
+            top: -8px;
+            width: 100%;
+            height: 3px;
+            border-radius: 4px;
+            background-color: #000000;
+        }
+        &::after{
+            content: '';
+            position: absolute;
+            top: 8px;
+            width: 100%;
+            height: 3px;
+            border-radius: 4px;
+            background-color: #000000;
+        }
+        @media (max-width: 784px) {
+            background-color: #FFFFFF;
+            &::before, &::after{
+                background-color: #FFFFFF;
+            }
+        }
+    }
+    @media (min-width: 1024px) {
+        div{
+            display: none !important;
+        }
     }
 `
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,216 @@
 import { NextPage } from 'next';
 import * as React from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { isThemeAtom } from 'recoil/theme';
 import styled from 'styled-components';
 
-const Home:NextPage=()=>{
-  return(
-    <h1>HELLO</h1>
-  )
+const Home:NextPage = () => {
+    const setDarkAtom = useSetRecoilState(isThemeAtom);
+    const isTheme = useRecoilValue(isThemeAtom);
+    React.useEffect(()=>{
+        // 
+    })
+    return(
+        <Container>
+            <AbsoluteBox>
+                <AbsoluteMainBox> 
+                    <div className="box" style={isTheme ? {transform : 'none'} : {transform: 'translateX(-50%)'}} data-aos="fade-up">
+                        <div className="wrapper blue">
+                            <img className="one" src="/images/KakaoTalk_Photo_2022-10-07-12-18-18.gif" alt="" />
+                        </div>
+                        <div className="wrapper red">
+                            <img className="two" src="/images/KakaoTalk_Photo_2022-10-07-12-18-26.gif" alt="" />
+                        </div>
+                    </div>
+                    <AbsoluteBtnBox>
+                      <div id="blue" className={isTheme ? 'focus' : ''} onClick={()=> setDarkAtom(true)}/>
+                      <div id="purple" className={isTheme ? '' : 'focus'} onClick={()=> setDarkAtom(false)}/>
+                    </AbsoluteBtnBox>
+                </AbsoluteMainBox>
+                <AbsoluteTitleBack>
+                      <h2>communicative<br/>developer</h2>
+                      <p><b style={ isTheme ? { color :"rgb(116, 185, 255)"} : {color : "#000000"}}>방문해주셔서 감사합니다</b><br/>웹개발을 전반적으로 다루는 개발 블로그가 되도록 노력하겠습니다</p>
+                </AbsoluteTitleBack>
+            </AbsoluteBox>
+            <Box color="#FFFFFF">
+
+            </Box>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+    scroll-snap-type: y mandatory;
+`
+const AbsoluteBox = styled.div`
+    position: sticky; 
+    width: 100%;
+    height: 100%;
+    top: 0;
+    scroll-snap-align: center;
+    z-index: 10;
+    h1{
+      font-size: 30px;
+      color: ${props=>props.theme.pointColor};
+    }
+`
+const AbsoluteMainBox = styled.div`
+  position: absolute;
+  width: 500px;
+  height: 100%;
+  left: 140px;
+  display: flex;
+  overflow: hidden;
+  .box{
+    width: auto;
+    height: 100vh;
+    display: flex;
+    transition: all ease-in-out .15s;
+    &#right{
+      transform: translateX(-500px);
+    }
+  }
+  @media (max-width: 1280px) {
+    left: 0;
+  }
+  .wrapper{
+    flex-shrink: 0;
+    width: 500px;
+    @media (max-width: 1024px) {
+      width: calc(100vw - 80px);
+    }
+    @media (max-width: 784px) {
+      width: 100vw;
+    }
+  }
+  img{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 0;
+    pointer-events: none;
+
+  }
+  .blue{
+    background-color: rgb(116, 185, 255);
+  }
+  .red{
+    background-color: #FFE616;
+  }
+  @media (max-width: 1280px) {
+    left: 0;
+  }
+  @media (max-width: 1024px) {
+    width: calc(100% - 80px);
+  }
+  @media (max-width: 784px) {
+    width: 100%;
+  }
+`
+
+const AbsoluteBtnBox = styled.div`
+    position: absolute;
+    left: 50%;
+    bottom: 40px;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    div{
+        width: 54px;
+        height: 6px;
+        background-color: #FFFFFF;
+        cursor: pointer;
+        &.focus#blue{
+            background-color: rgb(116, 185, 255);
+            cursor: auto;
+        }
+        &.focus#purple{
+            background-color: #FFE616;
+            cursor: auto;
+        }
+    }
+`
+
+const AbsoluteTitleBack = styled.div`
+    position: absolute;
+    top: calc(50%);
+    right: 0;
+    transform: translateY(-50%);
+    width: calc(100% - 680px);
+    @media (max-width: 1280px) {
+        width: calc(100% - 540px);
+    }
+    @media (max-width: 1024px) {
+        display: none;
+    }
+    h2{
+        position: absolute;
+        font-family: 'GmarketSansMedium';
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 80px;
+        line-height: 76px;
+        letter-spacing: -6px;
+        color: #f5f6f9;
+        pointer-events: none;
+        top: 50%;
+        transform: translateY(-50%);
+        @media (max-width: 1400px) {
+            font-size: 60px;
+            line-height: 56px;
+        }
+        @media (max-width: 1280px) {
+            font-size: 48px;
+            line-height: 42px;
+        }
+    }
+    p{
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      font-family: 'GmarketSansMedium';
+      font-size: 20px;
+      line-height: 27px;
+      b{
+        font-size: 44px;
+        line-height: 44px;
+        transition: all ease-in-out .15s;
+        color: ${props=>props.theme.pointColor};
+      }
+      @media (max-width: 1400px) {
+        font-size: 18px;
+        line-height: 22px;
+        b{
+          font-size: 36px;
+          line-height: 36px;
+          color: ${props=>props.theme.pointColor};
+        }
+      }
+      @media (max-width: 1280px) {
+        font-size: 16px;
+        line-height: 22px;
+      }
+    }
+`
+
+const Box = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    scroll-snap-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props=>props.color};
+    z-index: 15;
+`
 
 export default Home;
