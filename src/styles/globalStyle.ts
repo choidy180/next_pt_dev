9406b207fd2f4ddd8d94989f1d3204d9c4acae26
choidy180 <choidy180@naver.com>
2022-10-07 10:50:04 +0900

    commit

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -211,12 +211,6 @@ const GlobalStyle = createGlobalStyle`
         font-style: normal;
     }
     @font-face {
-        font-family: 'MabinogiClassicR';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2207-01@1.0/MabinogiClassicR.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-    @font-face {
         font-family: 'Pretendard-Regular';
         src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
         font-weight: 400;
