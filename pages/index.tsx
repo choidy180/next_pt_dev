47aa11835830e4447af82022c96864a31dcf8268
choidy180 <choidy180@naver.com>
2022-10-17 02:26:43 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,7 +121,7 @@ const AbsoluteMainBox = styled.div`
 const AbsoluteBtnBox = styled.div`
     position: absolute;
     left: 50%;
-    bottom: 120px;
+    bottom: 60px;
     transform: translateX(-50%);
     display: flex;
     justify-content: center;
