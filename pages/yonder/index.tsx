3279e9823c29034ddb4776dd58be68b5e7c44ea1
choidy180 <choidy180@naver.com>
2022-10-20 15:47:01 +0900

    commit

--- pages/yonder/index.tsx
+++ pages/yonder/index.tsx
@@ -35,6 +35,11 @@ export default function Yonder (){
                 <Head>당신을 영원히 기억하겠습니다</Head>
                 <HeadSub>온라인 추모관 Leftoverlife에 오신것을 환영합니다.<br/>당신의 기억들이 평안히 쉴 수 있는 공간이 되길 바라겠습니다.</HeadSub>
                 <Boundary/>
+                <ContentBox>
+                    <h2>기억하고자 하는 분의 이름을 입력해주세요</h2>
+                    <input type="text" placeholder="기억하고 싶은 이름"/>
+                    <button>다음</button>
+                </ContentBox>
             </TopBox>
         </Container>
     )
@@ -45,13 +50,23 @@ const TopBox = styled.div`
     min-height: calc(var(--vh, 1vh) * 100);
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     .yonderBack{
         filter: brightness(20%) ;
     }
 `
+const LogoImageBox = styled.div`
+    position: relative;
+    margin-top: 20vh;
+    width: 200px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 const Head = styled.p`
+    margin-top: 10.5px;
     color: #FFFFFF;
     font-size: 28px;
     text-shadow: 3px 3px rgb(52, 73, 94);
@@ -64,18 +79,59 @@ const HeadSub = styled.p`
     font-family: 'ChosunCentennial';
     text-align: center;
 `
-const LogoImageBox = styled.div`
-    position: relative;
-    width: 200px;
-    height: 200px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
 const Boundary = styled.div`
     width: calc(100% - 24px);
     max-width: 1200px;
     height: 1.4px;
     margin-top: 36px;
     background-color: white;
+`
+const ContentBox = styled.div`
+    margin-top: 32px;
+    min-height: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    h2{
+        text-align: center;
+        font-size: 22px;
+        line-height: 24px;
+        color: #FFFFFF;
+    }
+    input{
+        margin-top: 48px;
+        width: 100%;
+        min-width: 400px;
+        background-color: transparent;
+        padding: 5px 10.5px;
+        font-size: 20px;
+        border: none;
+        border-bottom: 2.4px solid rgb(127, 140, 141);
+        color: #FFFFFF;
+        &:after{
+            position: absolute;
+            z-index: 999999;
+            left: 0;
+            bottom: 0;
+            width: 100px;
+            height: 100px;
+            content: '123';
+            color: red;
+        }
+    }
+    button{
+        min-width: 400px;
+        margin-top: 38px;
+        font-size: 22px;
+        padding: 5px 10.5px;
+        color: #FFFFFF;
+        font-family: 'ChosunCentennial';
+        border: 1.4px solid rgb(127, 140, 141);
+        border-radius: 8px;
+        transition: all .15s ease-in-out;
+        &:hover{
+            border: 1.4px solid #FFFFFF;
+        }
+    }
 `
