7fa874b45abdedb189915718dc7af96e0c9b6598
choidy180 <choidy180@naver.com>
2022-10-27 23:40:53 +0900

    commit

--- components/creator/FirstBox.js
+++ components/creator/FirstBox.js
@@ -1,10 +1,38 @@
 import Image from "next/image"
+import { useRef, useState } from "react"
 import styled from "styled-components"
 import backImage from '../../public/images/5c16cf0cc47cdf6cfb39a74fbfdea6e0728ae8b9.jpg'
+import throttle from '../../util/horizonDrag';
 
 export default function CreatorFirstBox(){
+    const scrollRef = useRef(null);
+    const [isDrag, setIsDrag] = useState(false);
+    const [startX, setStartX] = useState();
+
+    const onDragStart = (e) => {
+        e.preventDefault();
+        setIsDrag(true);
+        setStartX(e.pageX + scrollRef.current.scrollLeft);
+    }
+    const onDragEnd = () => {
+        setIsDrag(false);
+    }
+
+    const onDragMove = (e) => {
+        if(isDrag){
+            scrollRef.current.scrollLeft = startX - e.pageX;
+        }
+    };
+    const delay = 100;
+    const onThrottleDragMove = throttle(onDragMove, delay);
     return(
-        <Wrapper>
+        <Wrapper 
+            ref={scrollRef}
+            onMouseDown={onDragStart}
+            onMouseMove={isDrag  ? onThrottleDragMove : null}
+            onMouseUp={onDragEnd}
+            onMouseLeave={onDragEnd}
+        >
             <Image
                 layout="fill"
                 objectFit="cover"
