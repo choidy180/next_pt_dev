c237af3b71330470e62be929393a4cd5a546cc2e
choidy180 <choidy180@naver.com>
2022-11-04 18:50:35 +0900

    commit

--- components/dnd/TodoExample.tsx
+++ components/dnd/TodoExample.tsx
@@ -27,9 +27,34 @@ export default function TodoExample({
     }, [setItems]);
 
     return (
-        <div className='p-4'>
-            <div className='mb-2'>
-                <h1 className='text-3xl font-bold'>Drag and Drop</h1>
+        <div className='p-4 bg-black'>
+            <div className='bg-gray-800 p-4'>
+                <h1 className='text-3xl font-bold text-white'>Drag and Drop</h1>
+                <span className='text-gray-200'>React with VanillaTS (might have a little bug...)</span>
+            </div>
+
+            <div className='mt-4 flex'>
+                <div className='todo grid flex-1 select-none grid-cols-2 gap-4 rounded-lg bg-slate-500 p-4'>
+                    {Object.keys(items).map((key) => (
+                        <div
+                            key={key}
+                            data-droppable-id={key}
+                            className='flex flex-col gap-3 rounded-xl bg-gray-200 p-4 ring-1 ring-gray-300 transition-shadow dark:bg-[#000000]'
+                        >
+                            <span className='text-xl font-semibold text-white'>{key.toLocaleUpperCase()}</span>
+                            {items[key as TItemStatus].map((item, index) => (
+                                <div
+                                    key={item.id}
+                                    data-index={index}
+                                    className='dnd-item rounded-lg bg-white p-4 transition-shadow dark:bg-slate-800 w-60'
+                                >
+                                    <h5 className='font-semibold text-white'>{item.title}</h5>
+                                    <span className='text-sm text-gray-300'>Make the world beautiful</span>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- pages/dnd.tsx
+++ pages/dnd.tsx
@@ -1,13 +1,38 @@
 import { Container } from 'components/Container';
 import DndPageBasic from 'components/dnd/basic';
 import TodoBasicPage from 'components/dnd/todoBasic';
+import TodoExample from 'components/dnd/TodoExample';
 import * as React from 'react';
 
+export type TItemStatus = 'todo' | 'doing';
+
+export type TItem = {
+  id: string;
+  status: TItemStatus;
+  title: string;
+  index: number;
+};
+
+export type TItems = {
+  [key in TItemStatus]: TItem[];
+};
+
+
 export default function DndPage(){
+    const [items, setItems] = React.useState<TItems>({
+        todo: [...Array(5)].map((_, i) => ({
+            id: `${i}${i}${i}`,
+            title: `Title ${i + 1}000`,
+            status: 'todo',
+            index: i,
+        })),
+        doing: [],
+    });
     return (
         <Container style={{gap: '20px'}}>
             <DndPageBasic/>
-            <TodoBasicPage/>
+            {/* <TodoBasicPage/> */}
+            <TodoExample items={items} setItems={setItems} />
         </Container>
     )
 }
